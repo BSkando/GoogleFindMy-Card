db7361f9e8f465ffb4810b111c860e03e196012b
Simon Smith <simonsmith5521@gmail.com>
2026-03-17 19:19:32 +0000

    add referrerPolicy: 'origin' to tiles for osm policy

--- googlefindmy-card.js
+++ googlefindmy-card.js
@@ -1506,7 +1506,8 @@ class GoogleFindMyCard extends LitElement {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '© OpenStreetMap contributors',
           maxZoom: 19,
-          errorTileUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=='
+          errorTileUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==',
+          referrerPolicy: 'origin'
         }).addTo(this._mapInstance);
 
         // Wait for tiles to render before invalidating size
